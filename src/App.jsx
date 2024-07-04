e2daa16251ebdeddd587e8d429816f08f1cacdc9
Nikita8Sannikov <Ak-4t7@mail.ru>
2024-07-04 18:50:17 +0600

    add conversation logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import {
   DEFAULT_BASE_CODE,
   DEFAULT_TARGET_CODE,
   DEFAULT_BASE_VALUE,
   DEFAULT_TARGET_VALUE,
+  DEFAULT_TO_FIXED
 } from "./constants";
 import  useExchangeRate  from "../hooks/useExchangeRate";
 import CurretnRow from "./components/CurrentRow";
@@ -12,7 +13,7 @@ import CurretnRow from "./components/CurrentRow";
 function App() {
   const [baseCode, setBaseCode] = useState(DEFAULT_BASE_CODE);
   const [targetCode, setTargetCode] = useState(DEFAULT_TARGET_CODE);
-  const [baseValue, setbaseValue] = useState(DEFAULT_BASE_VALUE)
+  const [baseValue, setBaseValue] = useState(DEFAULT_BASE_VALUE)
   const [targetValue, setTargetValue] = useState(DEFAULT_TARGET_VALUE)
   const { conversionRate } = useExchangeRate({
     baseCode,
@@ -20,18 +21,45 @@ function App() {
   });
 
   function onBaseValueChange(event) {
-    setbaseValue(event.target.value)
+    const value = Number(event.target.value)
+    setBaseValue(value)
+    const targetValue =
+      value && conversionRate
+        ? (value * conversionRate).toFixed(DEFAULT_TO_FIXED)
+        : null
+    setTargetValue(targetValue)
   }
   function onTargetValueChange(event) {
-    setTargetValue(event.target.value)
+    const value = Number(event.target.value)
+    setTargetValue(value)
+    const baseValue =
+      value && conversionRate
+        ? (value / conversionRate).toFixed(DEFAULT_TO_FIXED)
+        : null
+    setBaseValue(baseValue)
   }
 
   function onBaseCodeChange(value) {
+    setDefaultValues()
     setBaseCode(value)
   }
   function onTargetCodeChange(value) {
+    setDefaultValues()
     setTargetCode(value)
   }
+  function setDefaultValues(){
+    setBaseValue(DEFAULT_BASE_VALUE)
+    setTargetValue(DEFAULT_TARGET_VALUE)
+  }
+
+  useEffect(()=>{
+    if(conversionRate){
+      setTargetValue((baseValue*conversionRate)
+      .toFixed(DEFAULT_TO_FIXED)
+    )
+    }
+  }, [conversionRate])
+ 
   return (
     <>
      <div className="container">
@@ -39,11 +67,13 @@ function App() {
 
     <CurretnRow
     inputValue={baseValue}
+    selectedValue={baseCode}
     onInputChange={onBaseValueChange}
     onSelectChange={onBaseCodeChange}
     />
     <CurretnRow
     inputValue={targetValue}
+    selectedValue={targetCode}
     onInputChange={onTargetValueChange}
     onSelectChange={onTargetCodeChange}
     />
